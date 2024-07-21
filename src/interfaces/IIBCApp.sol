9fb08848b91f624254b94d6e379f3ccbecb4e33f
srdtrk <srdtrk@hotmail.com>
2024-07-22 02:02:18 +0800

    imp: added IBCApp interface

--- src/interfaces/IIBCApp.sol
+++ src/interfaces/IIBCApp.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.25;
 
-interface IIBCApp { }
+import { IIBCAppCallbacks } from "../msgs/IIBCAppCallbacks.sol";
+
+interface IIBCApp is IIBCAppCallbacks {
+    // @notice Called when a packet is to be sent to the counterparty chain.
+    // @param msg The callback message
+    function onSendPacket(OnSendPacketCallback calldata msg) external;
+
+    // @notice Called when a packet is received from the counterparty chain.
+    // @param msg The callback message
+    // @return The acknowledgement data
+    function onRecvPacket(OnRecvPacketCallback calldata msg) external returns (bytes memory);
+
+    // @notice Called when a packet acknowledgement is received from the counterparty chain.
+    // @param msg The callback message
+    function onAcknowledgementPacket(OnAcknowledgementPacketCallback calldata msg) external;
+
+    // @notice Called when a packet is timed out.
+    // @param msg The callback message
+    function onTimeoutPacket(OnTimeoutPacketCallback calldata msg) external;
+}
---
+++ src/msgs/IIBCAppCallbacks.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.25;
+
+import { IICS26RouterMsgs } from "./IICS26RouterMsgs.sol";
+
+interface IIBCAppCallbacks {
+    /// OnSendPacket is called when a packet send request is received by the router.
+    /// The packet send is cancelled if the callback response is an error.
+    struct OnSendPacketCallback {
+        /// The packet to be sent
+        IICS26RouterMsgs.Packet packet;
+        /// The sender of the packet
+        address sender;
+    }
+
+    /// Called when a packet is sent to this IBC application.
+    struct OnRecvPacketCallback {
+        /// The packet to be received
+        IICS26RouterMsgs.Packet packet;
+        /// The relayer of this message
+        address relayer;
+    }
+
+    /// Called when a packet is to be acknowledged by this IBC application.
+    /// This callback need not be responded with data.
+    struct OnAcknowledgementPacketCallback {
+        /// The packet to be acknowledged
+        IICS26RouterMsgs.Packet packet;
+        /// The acknowledgement
+        bytes acknowledgement;
+        /// The relayer of this message
+        address relayer;
+    }
+
+    /// Called when a packet is to be timed out by this IBC application.
+    /// This callback need not be responded with data.
+    struct OnTimeoutPacketCallback {
+        /// The packet to be timed out
+        IICS26RouterMsgs.Packet packet;
+        /// The relayer of this message
+        address relayer;
+    }
+}
