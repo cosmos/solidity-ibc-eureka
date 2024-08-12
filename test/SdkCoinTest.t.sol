4e9939007758f847c492290779f713d7abe733f3
srdtrk <srdtrk@hotmail.com>
2024-08-12 12:42:17 +0800

    imp: moved test errors

--- test/SdkCoinTest.t.sol
+++ test/SdkCoinTest.t.sol
@@ -606,4 +606,19 @@ contract SdkCoinTest is Test, IISdkCoinErrors {
         uint256 convertedAmount = SdkCoin._SdkCoinToERC20_ConvertAmount(address(tokenAddress), amount);
         invariant_SdkCointoERC20_ConvertedAmountBiggerOrEqualThanInput(convertedAmount, amount);
     }
+
+    ///////////////// Invariant Testing Errors
+    /// @param remainder The remainder that should be zero
+    error RemainderIsNotZero(uint256 remainder);
+
+    /// @param remainder The remainder that should be greater than zero
+    error RemainderIsNotBiggerThanZero(uint256 remainder);
+
+    /// @param convertedAmount The converted amount
+    /// @param amount The original amount
+    error ConvertedAmountNotEqualInput(uint64 convertedAmount, uint256 amount);
+
+    /// @param convertedAmount The converted amount
+    /// @param amount The original amount
+    error ConvertedAmountNotBiggerThanOrEqualInput(uint256 convertedAmount, uint64 amount);
 }
