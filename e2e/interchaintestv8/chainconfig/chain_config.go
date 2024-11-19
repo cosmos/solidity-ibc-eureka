dc5e454d689f77c194d667bace2d9663e577bf3b
Gjermund Garaba <gjermund@garaba.net>
2024-11-19 12:54:38 +0100

    switch to ibc-go eureka branch simapp

--- e2e/interchaintestv8/chainconfig/chain_config.go
+++ e2e/interchaintestv8/chainconfig/chain_config.go
@@ -14,8 +14,8 @@ var DefaultChainSpecs = []*interchaintest.ChainSpec{
 			ChainID: "simd-1",
 			Images: []ibc.DockerImage{
 				{
-					Repository: "ghcr.io/cosmos/ibc-go-wasm-simd",                        // FOR LOCAL IMAGE USE: Docker Image Name
-					Version:    "cian-issue-7510-ibcmodulev2-transfer-application-spike", // FOR LOCAL IMAGE USE: Docker Image Tag
+					Repository: "ghcr.io/cosmos/ibc-go-wasm-simd", // FOR LOCAL IMAGE USE: Docker Image Name
+					Version:    "feat-ibc-eureka",                 // FOR LOCAL IMAGE USE: Docker Image Tag
 					UidGid:     "1025:1025",
 				},
 			},
