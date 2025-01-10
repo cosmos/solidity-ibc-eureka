f8ed823dcf1e9ad80d89582794620034eb7d5445
Gjermund Garaba <gjermund@garaba.net>
2025-01-10 05:40:11 +0100

    deps: bump openzeppelin to 5.2.0 and replace our own impls (#209)
    Co-authored-by: srdtrk <srdtrk@hotmail.com>

--- contracts/utils/ICS20Lib.sol
+++ contracts/utils/ICS20Lib.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 // solhint-disable no-inline-assembly
 
 import { Strings } from "@openzeppelin/utils/Strings.sol";
+import { Bytes } from "@openzeppelin/utils/Bytes.sol";
 import { IICS20Errors } from "../errors/IICS20Errors.sol";
 import { IICS26RouterMsgs } from "../msgs/IICS26RouterMsgs.sol";
 import { IICS20TransferMsgs } from "../msgs/IICS20TransferMsgs.sol";
@@ -112,105 +113,17 @@ library ICS20Lib {
     /// @return address value
     /// @return true if the conversion was successful
     function hexStringToAddress(string memory addrHexString) internal pure returns (address, bool) {
-        bytes memory addrBytes = bytes(addrHexString);
-        if (addrBytes.length != 42) {
-            return (address(0), false);
-        } else if (addrBytes[0] != "0" || addrBytes[1] != "x") {
-            return (address(0), false);
-        }
-        uint256 addr = 0;
-        unchecked {
-            for (uint256 i = 2; i < 42; i++) {
-                uint256 c = uint256(uint8(addrBytes[i]));
-                if (c >= 48 && c <= 57) {
-                    addr = addr * 16 + (c - 48);
-                } else if (c >= 97 && c <= 102) {
-                    addr = addr * 16 + (c - 87);
-                } else if (c >= 65 && c <= 70) {
-                    addr = addr * 16 + (c - 55);
-                } else {
-                    return (address(0), false);
-                }
-            }
-        }
-        return (address(uint160(addr)), true);
+        (bool success, address addr) = Strings.tryParseAddress(addrHexString);
+        return (addr, success);
     }
 
     /// @notice mustHexStringToAddress converts a hex string to an address and reverts on failure.
     /// @param addrHexString hex address string
     /// @return address the converted address
     function mustHexStringToAddress(string memory addrHexString) internal pure returns (address) {
-        (address addr, bool success) = hexStringToAddress(addrHexString);
+        (bool success, address addr) = Strings.tryParseAddress(addrHexString);
         require(success, IICS20Errors.ICS20InvalidAddress(addrHexString));
         return addr;
-    }
-
-    /// @notice slice returns a slice of the original bytes from `start` to `start + length`.
-    /// @dev This is a copy from https://github.com/GNSPS/solidity-bytes-utils/blob/v0.8.0/contracts/BytesLib.sol
-    /// @param _bytes bytes
-    /// @param _start start index
-    /// @param _length length
-    /// @return sliced bytes
-    function slice(bytes memory _bytes, uint256 _start, uint256 _length) internal pure returns (bytes memory) {
-        if (_length + 31 < _length) {
-            revert IICS20Errors.ICS20BytesSliceOverflow(_length);
-        } else if (_start + _length > _bytes.length) {
-            revert IICS20Errors.ICS20BytesSliceOutOfBounds(_bytes.length, _start, _start + _length);
-        }
-
-        bytes memory tempBytes;
-
-        assembly {
-            switch iszero(_length)
-            case 0 {
-                // Get a location of some free memory and store it in tempBytes as
-                // Solidity does for memory variables.
-                tempBytes := mload(0x40)
-
-                // The first word of the slice result is potentially a partial
-                // word read from the original array. To read it, we calculate
-                // the length of that partial word and start copying that many
-                // bytes into the array. The first word we copy will start with
-                // data we don't care about, but the last `lengthmod` bytes will
-                // land at the beginning of the contents of the new array. When
-                // we're done copying, we overwrite the full first word with
-                // the actual length of the slice.
-                let lengthmod := and(_length, 31)
-
-                // The multiplication in the next line is necessary
-                // because when slicing multiples of 32 bytes (lengthmod == 0)
-                // the following copy loop was copying the origin's length
-                // and then ending prematurely not copying everything it should.
-                let mc := add(add(tempBytes, lengthmod), mul(0x20, iszero(lengthmod)))
-                let end := add(mc, _length)
-
-                for {
-                    // The multiplication in the next line has the same exact purpose
-                    // as the one above.
-                    let cc := add(add(add(_bytes, lengthmod), mul(0x20, iszero(lengthmod))), _start)
-                } lt(mc, end) {
-                    mc := add(mc, 0x20)
-                    cc := add(cc, 0x20)
-                } { mstore(mc, mload(cc)) }
-
-                mstore(tempBytes, _length)
-
-                //update free-memory pointer
-                //allocating the array padded to 32 bytes like the compiler does now
-                mstore(0x40, and(add(mc, 31), not(31)))
-            }
-            //if we want a zero-length slice let's just return a zero-length array
-            default {
-                tempBytes := mload(0x40)
-                //zero out the 32 bytes slice we are about to return
-                //we need to do it because Solidity does not garbage collect
-                mstore(tempBytes, 0)
-
-                mstore(0x40, add(tempBytes, 0x20))
-            }
-        }
-
-        return tempBytes;
     }
 
     /// @notice equal returns true if two byte arrays are equal.
@@ -229,7 +142,7 @@ library ICS20Lib {
         if (denomBz.length < prefix.length) {
             return false;
         }
-        return equal(slice(denomBz, 0, prefix.length), prefix);
+        return equal(Bytes.slice(denomBz, 0, prefix.length), prefix);
     }
 
     /// @notice errorAck returns an error acknowledgement.
