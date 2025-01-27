6e428d4a05f77cb28d05745de82e18cfa7a46c4b
srdtrk <srdtrk@hotmail.com>
2025-01-27 23:57:10 +0800

    ci: back to feat-eureka

--- e2e/interchaintestv8/chainconfig/chain_config.go
+++ e2e/interchaintestv8/chainconfig/chain_config.go
@@ -19,7 +19,7 @@ func IbcGoChainSpec(name, chainId string) *interchaintest.ChainSpec {
 			Images: []ibc.DockerImage{
 				{
 					Repository: "ghcr.io/cosmos/ibc-go-wasm-simd", // FOR LOCAL IMAGE USE: Docker Image Name
-					Version:    "gjermund-build-wasm-image-on-native-os",                  // FOR LOCAL IMAGE USE: Docker Image Tag
+					Version:    "feat-ibc-eureka",                 // FOR LOCAL IMAGE USE: Docker Image Tag
 					UidGid:     "1025:1025",
 				},
 			},
