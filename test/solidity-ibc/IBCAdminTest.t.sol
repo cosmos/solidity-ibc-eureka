734055958608e9e36b36707e03b35979e0b46886
srdtrk <srdtrk@hotmail.com>
2025-02-21 14:15:16 +0300

    imp: forge fmt

--- test/solidity-ibc/IBCAdminTest.t.sol
+++ test/solidity-ibc/IBCAdminTest.t.sol
@@ -188,7 +188,13 @@ contract IBCAdminTest is Test {
         assert(ics26Router.hasRole(ics26Router.PORT_CUSTOMIZER_ROLE(), portCustomizer));
 
         // Check that an unauthorized account cannot set the port
-        vm.expectRevert(abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, unauthorized, ics26Router.PORT_CUSTOMIZER_ROLE()));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector,
+                unauthorized,
+                ics26Router.PORT_CUSTOMIZER_ROLE()
+            )
+        );
         vm.prank(unauthorized);
         ics26Router.addIBCApp("newPort", address(ics20Transfer));
     }
