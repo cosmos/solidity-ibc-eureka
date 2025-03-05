a6294c245aa03c621d3fd1b9a67786d4feb71edf
srdtrk <srdtrk@hotmail.com>
2025-03-05 10:24:04 +0300

    fix: test

--- e2e/interchaintestv8/testvalues/values.go
+++ e2e/interchaintestv8/testvalues/values.go
@@ -95,7 +95,9 @@ const (
 	// SecondUniversalClientID is the second universal client ID. Used for testing.
 	SecondUniversalClientID = "client-1"
 	// CustomClientID is the custom client ID used for testing.
-	CustomClientID = "cosmoshub"
+	// BUG: https://github.com/cosmos/ibc-go/issues/8145
+	// We must use a client ID of the form `type-n` due to the issue above.
+	CustomClientID = "cosmoshub-1"
 )
 
 var (
