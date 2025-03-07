7c3cec1232395d8d1bd0a9970cd6069622b3bcf2
srdtrk <59252793+srdtrk@users.noreply.github.com>
2025-03-07 18:35:02 +0300

    feat: allow custom client ids (#362)
    Co-authored-by: Gjermund Garaba <gjermund@garaba.net>

--- contracts/errors/IICS02ClientErrors.sol
+++ contracts/errors/IICS02ClientErrors.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.28;
 
 interface IICS02ClientErrors {
-    /// @notice Invalid client type
-    /// @param clientType client type
-    error IBCInvalidClientType(string clientType);
+    /// @notice Invalid client id
+    /// @param clientId the invalid client identifier
+    error IBCInvalidClientId(string clientId);
 
     /// @notice Client not found
     /// @param clientId client identifier
@@ -13,6 +13,10 @@ interface IICS02ClientErrors {
     /// @notice Counterparty client not found
     /// @param counterpartyClientId counterparty client identifier
     error IBCCounterpartyClientNotFound(string counterpartyClientId);
+
+    /// @notice IBC client identifier already exists
+    /// @param clientId client identifier
+    error IBCClientAlreadyExists(string clientId);
 
     /// @notice Unreachable code
     error Unreachable();
