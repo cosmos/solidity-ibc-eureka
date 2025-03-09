99575b3411b9cf3656c78857989f6d3b93615da3
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2025-03-09 14:19:02 +0100

    feat(tests/scripts): modularize the deployments of contracts into libraries (#318)
    Co-authored-by: Gjermund Garaba <gjermund@garaba.net>

---
+++ scripts/helpers/Deployments.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { Vm } from "forge-std/Vm.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+
+abstract contract Deployments {
+    using stdJson for string;
+
+    string internal constant DEPLOYMENT_DIR = "/deployments/";
+
+    struct SP1ICS07TendermintDeployment {
+        // The verifier address can be set in the environment variables.
+        // If not set, then the verifier is set based on the zkAlgorithm.
+        // If set to "mock", then the verifier is set to a mock verifier.
+        address implementation;
+        string clientId;
+        string counterpartyClientId;
+        string verifier;
+        string[] merklePrefix;
+        bytes trustedClientState;
+        bytes trustedConsensusState;
+        bytes32 updateClientVkey;
+        bytes32 membershipVkey;
+        bytes32 ucAndMembershipVkey;
+        bytes32 misbehaviourVkey;
+    }
+
+    function loadSP1ICS07TendermintDeployment(
+        string memory json,
+        string memory key
+    )
+    public
+    view
+    returns (SP1ICS07TendermintDeployment memory) {
+        return SP1ICS07TendermintDeployment({
+            clientId: json.readStringOr(string.concat(key, ".clientId"), ""),
+            verifier: json.readStringOr(string.concat(key, ".verifier"), ""),
+            merklePrefix: json.readStringArrayOr(string.concat(key, ".merklePrefix"), new string[](0)),
+            counterpartyClientId: json.readStringOr(string.concat(key, ".counterpartyClientId"), ""),
+            implementation: json.readAddressOr(string.concat(key, ".implementation"), address(0)),
+            trustedClientState: json.readBytes(string.concat(key, ".trustedClientState")),
+            trustedConsensusState: json.readBytes(string.concat(key, ".trustedConsensusState")),
+            updateClientVkey: json.readBytes32(string.concat(key, ".updateClientVkey")),
+            membershipVkey: json.readBytes32(string.concat(key, ".membershipVkey")),
+            ucAndMembershipVkey: json.readBytes32(string.concat(key, ".ucAndMembershipVkey")),
+            misbehaviourVkey: json.readBytes32(string.concat(key, ".misbehaviourVkey"))
+        });
+    }
+
+    function loadSP1ICS07TendermintDeployments(
+        Vm vm,
+        string memory json
+    )
+    public
+    view
+    returns (SP1ICS07TendermintDeployment[] memory)
+    {
+        string[] memory keys = vm.parseJsonKeys(json, "$.light_clients");
+        SP1ICS07TendermintDeployment[] memory deployments = new SP1ICS07TendermintDeployment[](keys.length);
+
+        for (uint256 i = 0; i < keys.length; i++) {
+            string memory key = string.concat(".light_clients['", keys[i], "']");
+            deployments[i] = loadSP1ICS07TendermintDeployment(json, key);
+        }
+
+        return deployments;
+    }
+
+    struct ProxiedICS26RouterDeployment {
+        address implementation;
+        address proxy;
+        address timeLockAdmin;
+        address portCustomizer;
+    }
+
+    function loadProxiedICS26RouterDeployment(
+        Vm vm,
+        string memory json
+    )
+    public
+    pure
+    returns (ProxiedICS26RouterDeployment memory)
+    {
+        ProxiedICS26RouterDeployment memory fixture = ProxiedICS26RouterDeployment({
+            implementation: vm.parseJsonAddress(json, ".ics26Router.implementation"),
+            proxy: vm.parseJsonAddress(json, ".ics26Router.proxy"),
+            timeLockAdmin: vm.parseJsonAddress(json, ".ics26Router.timeLockAdmin"),
+            portCustomizer: vm.parseJsonAddress(json, ".ics26Router.portCustomizer")
+        });
+
+        return fixture;
+    }
+
+    struct ProxiedICS20TransferDeployment {
+        // transparant proxies
+        address ics26Router;
+
+        // implementation addresses
+        address implementation;
+        address escrowImplementation;
+        address ibcERC20Implementation;
+
+        // admin control
+        address pauser;
+        address permit2;
+        address proxy;
+    }
+
+    function loadProxiedICS20TransferDeployment(
+        Vm vm,
+        string memory json
+    )
+    public
+    pure
+    returns (ProxiedICS20TransferDeployment memory)
+    {
+        // abi.decode(vm.parseJson(json, ".ics20Transfer"), (ProxiedICS20TransferDeployment));
+        ProxiedICS20TransferDeployment memory fixture = ProxiedICS20TransferDeployment({
+            escrowImplementation: vm.parseJsonAddress(json, ".ics20Transfer.escrowImplementation"),
+            ibcERC20Implementation: vm.parseJsonAddress(json, ".ics20Transfer.ibcERC20Implementation"),
+            ics26Router: vm.parseJsonAddress(json, ".ics20Transfer.ics26Router"),
+            implementation: vm.parseJsonAddress(json, ".ics20Transfer.implementation"),
+            pauser: vm.parseJsonAddress(json, ".ics20Transfer.pauser"),
+            permit2: vm.parseJsonAddress(json, ".ics20Transfer.permit2"),
+            proxy: vm.parseJsonAddress(json, ".ics20Transfer.proxy")
+        });
+
+        return fixture;
+    }
+}
