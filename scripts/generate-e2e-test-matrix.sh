b0f88d80cb7709e6bbf53ba40df44b93ed154df8
Gjermund Garaba <gjermund@garaba.net>
2025-03-14 23:37:58 +0100

    fix matrix script

--- scripts/generate-e2e-test-matrix.sh
+++ scripts/generate-e2e-test-matrix.sh
@@ -3,7 +3,22 @@ set -e
 
 # Find all tests
 cd e2e/interchaintestv8
-TESTS=$(grep -R "func (s \*.*Suite) Test" . | sed -E 's/.*(Test[[:alnum:]_]+)\(.*/\1/')
+TESTS=$(grep -R "func (s \*.*Suite) Test" . | while read -r line; do
+  # Extract file name and suite name
+  file_name=$(echo "$line" | cut -d':' -f1)
+  suite_name=$(echo "$line" | sed -E 's/.*func \(s \*([[:alnum:]_]+)\).*/\1/')
+  test_name=$(echo "$line" | sed -E 's/.*(Test[[:alnum:]_]+)\(.*/\1/')
+  
+  # Find the top-level test function for this suite
+  top_level_test=$(grep -A1 -B1 "$suite_name" "$file_name" | grep "func Test" | head -1 | sed -E 's/.*func (Test[[:alnum:]_]+).*/\1/')
+  
+  # Output the combined test name
+  if [ -n "$top_level_test" ]; then
+    echo "$top_level_test/$test_name"
+  else
+    echo "$test_name"
+  fi
+done)
 
 # Convert to a JSON array
 JSON_ARRAY=$(echo "$TESTS" | jq -R . | jq -s .)
