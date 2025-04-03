e69f10002d8d8e226c1ec53033a56f1bc992aa05
srdtrk <srdtrk@hotmail.com>
2025-04-03 13:12:05 +0300

    imp: fixed build script

--- programs/relayer/build.rs
+++ programs/relayer/build.rs
@@ -6,6 +6,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .file_descriptor_set_path(&descriptor_path)
         .build_server(true)
-        .compile_protos(&["proto/relayer/relayer.proto"], &["proto/relayer"])?;
+        .compile_protos(
+            &["../../proto/relayer/relayer.proto"],
+            &["../../proto/relayer"],
+        )?;
     Ok(())
 }
