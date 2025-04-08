8c5ef558003f1bc3127c0f8c1dfdec7c0cac59f4
srdtrk <srdtrk@hotmail.com>
2025-04-08 15:26:09 +0300

    fix: eth genesis

--- packages/ethereum/types/src/consensus/genesis.rs
+++ packages/ethereum/types/src/consensus/genesis.rs
@@ -2,13 +2,16 @@
 
 use alloy_primitives::B256;
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 
 use super::fork::Version;
 
 /// Genesis provides information about the genesis of a chain.
+#[serde_as]
 #[derive(Serialize, Deserialize, PartialEq, Eq, Clone, Debug, Default)]
 pub struct Genesis {
     /// The genesis time (in unix seconds)
+    #[serde_as(as = "DisplayFromStr")]
     pub genesis_time: u64,
     /// The genesis validators root
     pub genesis_validators_root: B256,
