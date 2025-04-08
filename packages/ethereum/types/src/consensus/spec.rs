d94c68ebe15c10d9a4e7919cb090def44bbfbed6
srdtrk <srdtrk@hotmail.com>
2025-04-08 16:11:01 +0300

    fix: Spec

--- packages/ethereum/types/src/consensus/spec.rs
+++ packages/ethereum/types/src/consensus/spec.rs
@@ -1,44 +1,55 @@
 //! This module defines types related to Spec.
 
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 
 use super::fork::{Fork, ForkParameters, Version};
 
 /// The spec type, returned from the beacon api.
+#[serde_as]
 #[derive(Serialize, Deserialize, PartialEq, Eq, Clone, Debug, Default)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub struct Spec {
     /// The number of seconds per slot.
+    #[serde_as(as = "DisplayFromStr")]
     pub seconds_per_slot: u64,
     /// The number of slots per epoch.
+    #[serde_as(as = "DisplayFromStr")]
     pub slots_per_epoch: u64,
     /// The number of epochs per sync committee period.
+    #[serde_as(as = "DisplayFromStr")]
     pub epochs_per_sync_committee_period: u64,
 
     // Fork Parameters
     /// The genesis fork version.
     pub genesis_fork_version: Version,
     /// The genesis slot.
+    #[serde_as(as = "DisplayFromStr")]
     pub genesis_slot: u64,
     /// The altair fork version.
     pub altair_fork_version: Version,
     /// The altair fork epoch.
+    #[serde_as(as = "DisplayFromStr")]
     pub altair_fork_epoch: u64,
     /// The bellatrix fork version.
     pub bellatrix_fork_version: Version,
     /// The bellatrix fork epoch.
+    #[serde_as(as = "DisplayFromStr")]
     pub bellatrix_fork_epoch: u64,
     /// The capella fork version.
     pub capella_fork_version: Version,
     /// The capella fork epoch.
+    #[serde_as(as = "DisplayFromStr")]
     pub capella_fork_epoch: u64,
     /// The deneb fork version.
     pub deneb_fork_version: Version,
     /// The deneb fork epoch.
+    #[serde_as(as = "DisplayFromStr")]
     pub deneb_fork_epoch: u64,
     /// The electra fork version.
     pub electra_fork_version: Version,
     /// The electra fork epoch.
+    #[serde_as(as = "DisplayFromStr")]
     pub electra_fork_epoch: u64,
 }
 
