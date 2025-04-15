54233f5953ee1770a0975212217a6b77f184c94c
srdtrk <srdtrk@hotmail.com>
2025-04-15 15:01:10 +0300

    imp: save progress

---
+++ contracts/ICS27GMP.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/// @title ICS27 General Message Passing
+/// @notice This contract is the implementation of the ics27-2 IBC specification for general message passing.
+contract ICS27GMP {}
