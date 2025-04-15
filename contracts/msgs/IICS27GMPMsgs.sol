f7e0310817d96f04e513bfdfb1e3f644c49b496c
srdtrk <srdtrk@hotmail.com>
2025-04-15 12:16:29 +0300

    imp: impprove packet data
    (cherry picked from commit f56489b0c129c8f94bffac94fcc941b5dfa6b1b6)

--- contracts/msgs/IICS27GMPMsgs.sol
+++ contracts/msgs/IICS27GMPMsgs.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-interface IContractCallsMsgs {
-    /// @notice ContractCallsPacketData is the payload for the contract call application.
+interface IICS27GMPMsgs {
+    /// @notice GMPPacketData is the payload for the GMP application.
     /// @param sender The sender of the packet
-    /// @param receiver The receiver contract of the call
+    /// @param receiver The receiver address of the contract call
+    /// @param salt The salt used to generate the caller account address
     /// @param payload The payload of the call
     /// @param memo Optional memo
-    struct ContractCallsPacketData {
+    struct GMPPacketData {
         string sender;
         string receiver;
+        bytes salt;
         bytes payload;
         string memo;
     }
