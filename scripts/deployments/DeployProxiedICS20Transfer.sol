14dde6896d1b3d360a300876367d33a1de48704f
srdtrk <srdtrk@hotmail.com>
2025-05-07 20:51:20 +0400

    chore: lint

--- scripts/deployments/DeployProxiedICS20Transfer.sol
+++ scripts/deployments/DeployProxiedICS20Transfer.sol
@@ -6,18 +6,8 @@ pragma solidity ^0.8.28;
 // solhint-disable-next-line no-global-import
 import "forge-std/console.sol";
 
-import { Script } from "forge-std/Script.sol";
 import { ERC1967Proxy } from "@openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import { IBeacon } from "@openzeppelin-contracts/proxy/beacon/IBeacon.sol";
-import { ERC1967Utils } from "@openzeppelin-contracts/proxy/ERC1967/ERC1967Utils.sol";
 import { ICS20Transfer } from "../../contracts/ICS20Transfer.sol";
-import { IICS26Router } from "../../contracts/interfaces/IICS26Router.sol";
-import { ICS20Lib } from "../../contracts/utils/ICS20Lib.sol";
-import { IBCERC20 } from "../../contracts/utils/IBCERC20.sol";
-import { Escrow } from "../../contracts/utils/Escrow.sol";
-import { IBCPausableUpgradeable } from "../../contracts/utils/IBCPausableUpgradeable.sol";
-import { Strings } from "@openzeppelin-contracts/utils/Strings.sol";
-
 
 library DeployProxiedICS20Transfer {
     function deployProxiedICS20Transfer(
--- scripts/deployments/DeployProxiedICS26Router.sol
+++ scripts/deployments/DeployProxiedICS26Router.sol
@@ -3,13 +3,8 @@ pragma solidity ^0.8.28;
 
 // solhint-disable custom-errors,gas-custom-errors
 
-import { stdJson } from "forge-std/StdJson.sol";
 import { ICS26Router } from "../../contracts/ICS26Router.sol";
-import { IIBCUUPSUpgradeable } from "../../contracts/interfaces/IIBCUUPSUpgradeable.sol";
-import { Strings } from "@openzeppelin-contracts/utils/Strings.sol";
 import { ERC1967Proxy } from "@openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import { ERC1967Utils } from "@openzeppelin-contracts/proxy/ERC1967/ERC1967Utils.sol";
-import { Script } from "forge-std/Script.sol";
 
 library DeployProxiedICS26Router {
     function deployProxiedICS26Router(
