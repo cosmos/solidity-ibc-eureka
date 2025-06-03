ef3b8950b71c631d65044a14a8b18bff73e37c66
srdtrk <srdtrk@hotmail.com>
2025-06-03 18:51:27 +0400

    lint

--- contracts/utils/IBCCallbackReceiver.sol
+++ contracts/utils/IBCCallbackReceiver.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import { IIBCAppCallbacks } from "../msgs/IIBCAppCallbacks.sol";
 import { IIBCCallbacks } from "../interfaces/IIBCCallbacks.sol";
 
 import { ERC165 } from "@openzeppelin-contracts/utils/introspection/ERC165.sol";
