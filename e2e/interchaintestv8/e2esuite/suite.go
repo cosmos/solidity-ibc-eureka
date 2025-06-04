0a41605412aac28ba5e590ec038dcd4a745d644f
Aditya <14364734+AdityaSripal@users.noreply.github.com>
2025-06-04 19:08:11 +0200

    feat(eth): add support for historical updates (#503)
    Co-authored-by: Gjermund Garaba <gjermund@garaba.net>

--- e2e/interchaintestv8/e2esuite/suite.go
+++ e2e/interchaintestv8/e2esuite/suite.go
@@ -39,10 +39,19 @@ type TestSuite struct {
 
 	// proposalIDs keeps track of the active proposal ID for cosmos chains
 	proposalIDs map[string]uint64
+	// WasmLightClientTag decides which version of the eth light client to use.
+	// Either an empty string, or 'local', means it will use the local binary in the repo, unless running in mock mode
+	// otherwise, it will download the version from the github release with the given tag
+	WasmLightClientTag string
 }
 
 // SetupSuite sets up the chains, relayer, user accounts, clients, and connections
 func (s *TestSuite) SetupSuite(ctx context.Context) {
+	// To let the download version be overridden by a calling test
+	if s.WasmLightClientTag == "" {
+		s.WasmLightClientTag = os.Getenv(testvalues.EnvKeyE2EWasmLightClientTag)
+	}
+
 	icChainSpecs := chainconfig.DefaultChainSpecs
 
 	s.ethTestnetType = os.Getenv(testvalues.EnvKeyEthTestnetType)
