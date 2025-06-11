eeeae7a2eb974d4cac87bcbf572d636972189023
srdtrk <srdtrk@hotmail.com>
2025-06-11 12:44:58 +0400

    imp: move selectors to lib

--- scripts/deployments/DeployAccessManagerWithRoles.sol
+++ scripts/deployments/DeployAccessManagerWithRoles.sol
@@ -3,39 +3,8 @@ pragma solidity ^0.8.28;
 
 import { IBCRolesLib } from "../../contracts/utils/IBCRolesLib.sol";
 import { IAccessManager } from "@openzeppelin-contracts/access/manager/IAccessManager.sol";
-import { IICS26RouterAccessControlled } from "../../contracts/interfaces/IICS26Router.sol";
-import { IICS20TransferAccessControlled } from "../../contracts/interfaces/IICS20Transfer.sol";
-import { IICS02ClientAccessControlled } from "../../contracts/interfaces/IICS02Client.sol";
-import { IIBCPausable } from "../../contracts/interfaces/IIBCPausable.sol";
-import { UUPSUpgradeable } from "@openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
 
 abstract contract DeployAccessManagerWithRoles {
-    bytes4[] public ics26IdCustomizerFunctions =
-        [IICS26RouterAccessControlled.addIBCApp.selector, IICS02ClientAccessControlled.addClient.selector];
-    bytes4[] public ics26RelayerFunctions = [
-        IICS26RouterAccessControlled.recvPacket.selector,
-        IICS26RouterAccessControlled.timeoutPacket.selector,
-        IICS26RouterAccessControlled.ackPacket.selector,
-        IICS02ClientAccessControlled.updateClient.selector
-    ];
-    bytes4[] public pauserFunctions = [IIBCPausable.pause.selector];
-    bytes4[] public unpauserFunctions = [IIBCPausable.unpause.selector];
-    bytes4[] public erc20CustomizerFunctions = [IICS20TransferAccessControlled.setCustomERC20.selector];
-    bytes4[] public delegateSenderFunctions = [IICS20TransferAccessControlled.sendTransferWithSender.selector];
-    /**
-     * @notice The upgrade functions are not restricted to a specific role.
-     * Therefore they can only be called by the `ADMIN_ROLE`.
-     */
-    bytes4[] public upgradeFunctions = [UUPSUpgradeable.upgradeToAndCall.selector];
-    /**
-     * @notice The upgrade functions are not restricted to a specific role.
-     * Therefore they can only be called by the `ADMIN_ROLE`.
-     */
-    bytes4[] public beaconUpgradeFunctions = [
-        IICS20TransferAccessControlled.upgradeEscrowTo.selector,
-        IICS20TransferAccessControlled.upgradeIBCERC20To.selector
-    ];
-
     function accessManagerSetTargetRoles(
         IAccessManager accessManager,
         address ics26,
@@ -44,16 +13,16 @@ abstract contract DeployAccessManagerWithRoles {
     )
         public
     {
-        accessManager.setTargetFunctionRole(ics26, ics26IdCustomizerFunctions, IBCRolesLib.ID_CUSTOMIZER_ROLE);
-        accessManager.setTargetFunctionRole(ics26, ics26RelayerFunctions, IBCRolesLib.RELAYER_ROLE);
-        accessManager.setTargetFunctionRole(ics20, pauserFunctions, IBCRolesLib.PAUSER_ROLE);
-        accessManager.setTargetFunctionRole(ics20, unpauserFunctions, IBCRolesLib.UNPAUSER_ROLE);
-        accessManager.setTargetFunctionRole(ics20, erc20CustomizerFunctions, IBCRolesLib.ERC20_CUSTOMIZER_ROLE);
-        accessManager.setTargetFunctionRole(ics20, delegateSenderFunctions, IBCRolesLib.DELEGATE_SENDER_ROLE);
+        accessManager.setTargetFunctionRole(ics26, IBCRolesLib.ics26IdCustomizerSelectors(), IBCRolesLib.ID_CUSTOMIZER_ROLE);
+        accessManager.setTargetFunctionRole(ics26, IBCRolesLib.ics26RelayerSelectors(), IBCRolesLib.RELAYER_ROLE);
+        accessManager.setTargetFunctionRole(ics20, IBCRolesLib.pauserSelectors(), IBCRolesLib.PAUSER_ROLE);
+        accessManager.setTargetFunctionRole(ics20, IBCRolesLib.unpauserSelectors(), IBCRolesLib.UNPAUSER_ROLE);
+        accessManager.setTargetFunctionRole(ics20, IBCRolesLib.erc20CustomizerSelectors(), IBCRolesLib.ERC20_CUSTOMIZER_ROLE);
+        accessManager.setTargetFunctionRole(ics20, IBCRolesLib.delegateSenderSelectors(), IBCRolesLib.DELEGATE_SENDER_ROLE);
         // TODO: fix rate limiter role
 
         if (pubRelay) {
-            accessManager.setTargetFunctionRole(ics26, ics26RelayerFunctions, IBCRolesLib.PUBLIC_ROLE);
+            accessManager.setTargetFunctionRole(ics26, IBCRolesLib.ics26RelayerSelectors(), IBCRolesLib.PUBLIC_ROLE);
         }
     }
 
