763cfb9a345da7795c03bb69e2d678235d1e2d2f
srdtrk <srdtrk@hotmail.com>
2025-06-13 14:43:24 +0400

    imp: converted public lib constants internal

--- contracts/utils/IBCRolesLib.sol
+++ contracts/utils/IBCRolesLib.sol
@@ -12,31 +12,31 @@ import { UUPSUpgradeable } from "@openzeppelin-contracts/proxy/utils/UUPSUpgrade
 /// @notice This library is used to define the shared roles for IBC contracts.
 library IBCRolesLib {
     /// @notice The admin role as per defined by AccessManager.
-    uint64 public constant ADMIN_ROLE = type(uint64).min;
+    uint64 internal constant ADMIN_ROLE = type(uint64).min;
 
     /// @notice The relayer role as per defined by AccessManager.
-    uint64 public constant PUBLIC_ROLE = type(uint64).max;
+    uint64 internal constant PUBLIC_ROLE = type(uint64).max;
 
     /// @notice Only addresses with this role may relay packets.
-    uint64 public constant RELAYER_ROLE = 1;
+    uint64 internal constant RELAYER_ROLE = 1;
 
     /// @notice The pauser role can pause the ICS20Transfer application.
-    uint64 public constant PAUSER_ROLE = 2;
+    uint64 internal constant PAUSER_ROLE = 2;
 
     /// @notice The unpauser role can unpause the ICS20Transfer application.
-    uint64 public constant UNPAUSER_ROLE = 3;
+    uint64 internal constant UNPAUSER_ROLE = 3;
 
     /// @notice Has permission to call `ICS20Transfer.sendTransferWithSender`.
-    uint64 public constant DELEGATE_SENDER_ROLE = 4;
+    uint64 internal constant DELEGATE_SENDER_ROLE = 4;
 
     /// @notice Can set withdrawal rate limits per ERC20 token.
-    uint64 public constant RATE_LIMITER_ROLE = 5;
+    uint64 internal constant RATE_LIMITER_ROLE = 5;
 
     /// @notice Can set custom port ids and client ids in ICS26Router.
-    uint64 public constant ID_CUSTOMIZER_ROLE = 6;
+    uint64 internal constant ID_CUSTOMIZER_ROLE = 6;
 
     /// @notice Can set custom ERC20 contracts for IBC denoms in ICS20Transfer.
-    uint64 public constant ERC20_CUSTOMIZER_ROLE = 7;
+    uint64 internal constant ERC20_CUSTOMIZER_ROLE = 7;
 
     /// @notice The functions that can be called by the RELAYER_ROLE in ICS26Router.
     /// @return An array of function selectors that can be called by the RELAYER_ROLE.
