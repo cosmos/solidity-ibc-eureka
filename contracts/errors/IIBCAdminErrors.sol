78f436d1e66df894ea42a652690eb6893c0b73fa
srdtrk <srdtrk@hotmail.com>
2025-06-13 19:48:58 +0400

    imp: remove IBCAdmin

--- contracts/errors/IIBCAdminErrors.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-/// @title IIBCAdminErrors
-/// @notice Interface for IBCAdmin contract errors
-interface IIBCAdminErrors {
-    /// @notice Error returned when caller is not the timelocked admin nor the governance admin
-    error Unauthorized();
-}
