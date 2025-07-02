745d04b430472f1bd3649eb0d597c4ebbee9273c
johnnylarner <james.richardson.2556@gmail.com>
2025-07-02 16:17:38 +0200

    test: add store tests

--- programs/ibc-attestor/src/attestation_store.rs
+++ programs/ibc-attestor/src/attestation_store.rs
@@ -41,3 +41,130 @@ impl AttestationStore {
         self.store.iter().filter(move |(k, _)| k >= &height)
     }
 }
+
+#[cfg(test)]
+mod constructor {
+    use super::*;
+
+    #[test]
+    fn calcs_max_entries_correctly() {
+        let store = AttestationStore::new(9_000);
+        assert_eq!(store.max_entries, 10);
+    }
+}
+
+#[cfg(test)]
+mod push {
+    use super::*;
+
+    #[test]
+    fn does_not_add_duplicate_heights_but_adds_new_height() {
+        let mut store = AttestationStore::new(9_000);
+
+        for i in 1..=10 {
+            let att = Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            };
+            store.push(i, att);
+        }
+
+        assert_eq!(store.store.len(), 10);
+
+        store.push(
+            10,
+            Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            },
+        );
+        store.push(
+            10,
+            Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            },
+        );
+        assert_eq!(store.store.len(), 10);
+        assert_eq!(store.store.back().map(|(h, _)| h), Some(&10));
+
+        store.push(
+            11,
+            Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            },
+        );
+        assert_eq!(store.store.len(), 10);
+        assert_eq!(store.store.back().map(|(h, _)| h), Some(&11));
+    }
+}
+
+#[cfg(test)]
+mod range_from {
+    use super::*;
+
+    #[test]
+    fn returns_all_heights() {
+        let mut store = AttestationStore::new(9_000);
+
+        for i in 1..=10 {
+            let att = Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            };
+            store.push(i, att);
+        }
+        let range = store.range_from(0);
+
+        assert_eq!(range.count(), 10);
+    }
+
+    #[test]
+    fn returns_half_of_all_heights() {
+        let mut store = AttestationStore::new(9_000);
+
+        for i in 1..=10 {
+            let att = Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            };
+            store.push(i, att);
+        }
+        let range = store.range_from(6);
+
+        assert_eq!(range.count(), 5);
+    }
+
+    #[test]
+    fn returns_latest_height() {
+        let mut store = AttestationStore::new(9_000);
+
+        for i in 1..=10 {
+            let att = Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            };
+            store.push(i, att);
+        }
+        let range = store.range_from(10);
+
+        assert_eq!(range.count(), 1);
+    }
+
+    #[test]
+    fn no_heights() {
+        let mut store = AttestationStore::new(9_000);
+
+        for i in 1..=10 {
+            let att = Attestation {
+                signature: [0; 64],
+                data: Vec::new(),
+            };
+            store.push(i, att);
+        }
+        let range = store.range_from(11);
+
+        assert_eq!(range.count(), 0);
+    }
+}
