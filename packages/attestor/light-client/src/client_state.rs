00621151363b3acbd76930f507bad7cc1f7dfa25
johnnylarner <james.richardson.2556@gmail.com>
2025-07-18 13:24:13 +0200

    feat: add public keys to client

--- packages/attestor/light-client/src/client_state.rs
+++ packages/attestor/light-client/src/client_state.rs
@@ -1,11 +1,14 @@
 //! Attestor client state for IBC light client
 
+use secp256k1::PublicKey;
 use serde::{Deserialize, Serialize};
 
 /// Minimal attestor client state for IBC light client
 /// Contains only the essential information needed for client management
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ClientState {
+    /// Attestor public keys
+    pub pub_keys: [PublicKey; 5],
     /// Latest height for tracking progression
     pub latest_height: u64,
     /// Whether the client is frozen due to misbehavior
