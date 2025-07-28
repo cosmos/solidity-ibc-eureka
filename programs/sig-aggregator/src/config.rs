e39136db4d5983d6010b2de7bdaa6a6f583d91d9
tamjidahmed <mail.to.tamjid@gmail.com>
2025-07-29 00:15:18 +0600

    fix lint

--- programs/sig-aggregator/src/config.rs
+++ programs/sig-aggregator/src/config.rs
@@ -17,8 +17,8 @@ impl Config {
         let content = fs::read_to_string(path)
             .with_context(|| format!("Failed to read config file '{}'", path.display()))?;
 
-        let config: Config = toml::from_str(&content)
-            .context("Failed to parse TOML configuration")?;
+        let config: Config = 
+            toml::from_str(&content).context("Failed to parse TOML configuration")?;
 
         config.validate()?;
 
