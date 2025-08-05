1f60642d87a8be9057cac984bef44098f4f496c8
Gjermund Garaba <gjermund@garaba.net>
2025-08-05 12:34:51 +0200

    added basic e2e relayer config options

---
+++ e2e/interchaintestv8/relayer/attested_to_cosmos_config.go
@@ -0,0 +1,33 @@
+package relayer
+
+// AttestedToCosmosConfigInfo is a struct that holds the configuration information for the Attested to Cosmos config template
+type AttestedToCosmosConfigInfo struct {
+	// Attested chain identifier
+	AttestedChainID string
+	// Cosmos chain identifier
+	CosmosChainID string
+	// Aggregator service URL for fetching attestations
+	AggregatorUrl string
+	// Cosmos chain Tendermint RPC URL
+	CosmosTmRPC string
+	// Cosmos chain submitter address
+	CosmosSignerAddress string
+}
+
+func CreateAttestedToCosmosModules(
+	configInfo AttestedToCosmosConfigInfo,
+) []ModuleConfig {
+	return []ModuleConfig{
+		{
+			Name:     ModuleAttestedToCosmos,
+			SrcChain: configInfo.AttestedChainID,
+			DstChain: configInfo.CosmosChainID,
+			Config: AttestedToCosmosModuleConfig{
+				AggregatorUrl: configInfo.AggregatorUrl,
+				TargetRpcUrl:  configInfo.CosmosTmRPC,
+				SignerAddress: configInfo.CosmosSignerAddress,
+				ChainId:       configInfo.AttestedChainID,
+			},
+		},
+	}
+}
