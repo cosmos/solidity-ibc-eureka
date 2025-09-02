e60de83bd61e296e793818f04f8a8821e58cbd32
srdtrk <59252793+srdtrk@users.noreply.github.com>
2025-09-02 16:49:05 +0400

    chore: script to deploy AttestorLightClient (#732)

---
+++ scripts/AttestorLCDeploy.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/*
+    This script is used for deploying the demo contracts on testnets
+*/
+
+// solhint-disable custom-errors,gas-custom-errors
+
+import { Script } from "forge-std/Script.sol";
+
+import { AttestorLightClient } from "../contracts/light-clients/AttestorLightClient.sol";
+
+contract AttestorLCDeploy is Script {
+    function run() public {
+        address[] memory attestors = new address[](1);
+        attestors[0] = 0x5Bc9C9baB1B5Df2d114164437261839D207AF061;
+
+        vm.startBroadcast();
+
+        // Deploy Attestor Light Client with proxy
+        new AttestorLightClient(attestors, 1, 7371, 1_756_814_141, address(0));
+
+        vm.stopBroadcast();
+    }
+}
