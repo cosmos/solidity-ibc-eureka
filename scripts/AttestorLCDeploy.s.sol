5b3adc67668f90b68f210d21cba55c8a52a4ce11
srdtrk <srdtrk@hotmail.com>
2025-09-02 16:34:38 +0400

    chore: simplify

--- scripts/AttestorLCDeploy.s.sol
+++ scripts/AttestorLCDeploy.s.sol
@@ -7,25 +7,11 @@ pragma solidity ^0.8.28;
 
 // solhint-disable custom-errors,gas-custom-errors
 
-import { stdJson } from "forge-std/StdJson.sol";
 import { Script } from "forge-std/Script.sol";
 
-import { ICS26Router } from "../contracts/ICS26Router.sol";
-import { ICS27GMP } from "../contracts/ICS27GMP.sol";
-import { Strings } from "@openzeppelin-contracts/utils/Strings.sol";
-import { ICS27Lib } from "../contracts/utils/ICS27Lib.sol";
-import { ERC1967Proxy } from "@openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import { ICS27Account } from "../contracts/utils/ICS27Account.sol";
-import { AccessManager } from "@openzeppelin-contracts/access/manager/AccessManager.sol";
-import { IBCXERC20 } from "../contracts/demo/IBCXERC20.sol";
 import { AttestorLightClient } from "../contracts/light-clients/AttestorLightClient.sol";
 
 contract AttestorLCDeploy is Script {
-    using stdJson for string;
-
-    string internal constant DEPLOYMENTS_DIR = "./scripts/deployments/";
-
-    // solhint-disable-next-line function-max-lines
     function run() public {
 	address[] memory attestors = new address[](1);
 	attestors[0] = 0x5Bc9C9baB1B5Df2d114164437261839D207AF061;
