b7b7c4609eee53dccecae87a08169085855854be
srdtrk <srdtrk@hotmail.com>
2025-09-02 16:48:41 +0400

    imp: forge fmt

--- scripts/AttestorLCDeploy.s.sol
+++ scripts/AttestorLCDeploy.s.sol
@@ -13,21 +13,14 @@ import { AttestorLightClient } from "../contracts/light-clients/AttestorLightCli
 
 contract AttestorLCDeploy is Script {
     function run() public {
-	address[] memory attestors = new address[](1);
-	attestors[0] = 0x5Bc9C9baB1B5Df2d114164437261839D207AF061;
+        address[] memory attestors = new address[](1);
+        attestors[0] = 0x5Bc9C9baB1B5Df2d114164437261839D207AF061;
 
         vm.startBroadcast();
 
-	// Deploy Attestor Light Client with proxy
-	new AttestorLightClient(
-		attestors,
-		1,
-		7371,
-		1756814141,
-		address(0)
-	);
+        // Deploy Attestor Light Client with proxy
+        new AttestorLightClient(attestors, 1, 7371, 1_756_814_141, address(0));
 
         vm.stopBroadcast();
     }
 }
-
