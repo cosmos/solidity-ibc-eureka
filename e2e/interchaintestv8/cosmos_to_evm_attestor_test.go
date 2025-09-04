f6f21c131c1855040f7e69caca9ee9326d3c2b1a
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-09-04 15:55:48 +0200

    Deploy ibc contracts, start the relayer

--- e2e/interchaintestv8/cosmos_to_evm_attestor_test.go
+++ e2e/interchaintestv8/cosmos_to_evm_attestor_test.go
@@ -9,10 +9,20 @@ import (
 	"github.com/cosmos/interchaintest/v10/ibc"
 	"github.com/stretchr/testify/require"
 
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	"github.com/cosmos/solidity-ibc-eureka/packages/go-abigen/ics20transfer"
+	"github.com/cosmos/solidity-ibc-eureka/packages/go-abigen/ics26router"
+
 	"github.com/srdtrk/solidity-ibc-eureka/e2e/v8/attestor"
 	"github.com/srdtrk/solidity-ibc-eureka/e2e/v8/e2esuite"
+	"github.com/srdtrk/solidity-ibc-eureka/e2e/v8/ethereum"
+	"github.com/srdtrk/solidity-ibc-eureka/e2e/v8/relayer"
 	tv "github.com/srdtrk/solidity-ibc-eureka/e2e/v8/testvalues"
 	attestortypes "github.com/srdtrk/solidity-ibc-eureka/e2e/v8/types/attestor"
+	"github.com/srdtrk/solidity-ibc-eureka/e2e/v8/types/erc20"
+	relayertypes "github.com/srdtrk/solidity-ibc-eureka/e2e/v8/types/relayer"
 )
 
 // TestCosmosToEVMAttestor E2E test that exercises Cosmos state attestation on EVM chain using
@@ -52,10 +62,17 @@ type cosmosToEVMAttestorTestSuite struct {
 
 	// clients
 	attestorClient attestortypes.AttestationServiceClient
+	relayerClient  relayertypes.RelayerServiceClient
+
+	// evmContracts
+	evmContracts evmContracts
 }
 
 func newCosmosToEVMAttestorTestSuite(t *testing.T) *cosmosToEVMAttestorTestSuite {
 	t.Helper()
+
+	// note: this is really bad, but other tests internals expect chdir
+	require.NoError(t, os.Chdir("../.."))
 
 	ctx := context.Background()
 
@@ -95,15 +112,20 @@ func newCosmosToEVMAttestorTestSuite(t *testing.T) *cosmosToEVMAttestorTestSuite
 	base.SetT(t)
 	base.SetupSuite(ctx)
 
+	var (
+		evmChain    = base.EthChain
+		cosmosChain = base.CosmosChains[0]
+	)
+
 	// Set some ENV related to RPC
-	os.Setenv(tv.EnvKeyEthRPC, base.EthChain.RPC)
-	os.Setenv(tv.EnvKeyTendermintRPC, base.CosmosChains[0].GetHostRPCAddress())
+	os.Setenv(tv.EnvKeyEthRPC, evmChain.RPC)
+	os.Setenv(tv.EnvKeyTendermintRPC, cosmosChain.GetHostRPCAddress())
 
 	// 3. Provision users
-	evmDeployer, err := base.EthChain.CreateAndFundUser()
+	evmDeployer, err := evmChain.CreateAndFundUser()
 	require.NoError(t, err, "unable to provision EVM deployer")
 
-	cosmosSender := base.CreateAndFundCosmosUser(ctx, base.CosmosChains[0])
+	cosmosSender := base.CreateAndFundCosmosUser(ctx, cosmosChain)
 
 	// 4. Setup ONE cosmos attestor (for the sake of the current test)
 	// TODO: support for arbitrary number of attestors in the future with
@@ -112,7 +134,7 @@ func newCosmosToEVMAttestorTestSuite(t *testing.T) *cosmosToEVMAttestorTestSuite
 
 	attestorAddr := runAttestor(t, attestor.CosmosBinary, func(c *attestor.AttestorConfig) string {
 		c.Server.Port = attestorPort
-		c.Cosmos.URL = base.CosmosChains[0].GetHostRPCAddress()
+		c.Cosmos.URL = cosmosChain.GetHostRPCAddress()
 
 		return "/tmp/attestor_0.toml"
 	})
@@ -120,9 +142,30 @@ func newCosmosToEVMAttestorTestSuite(t *testing.T) *cosmosToEVMAttestorTestSuite
 	attestorClient, err := attestor.GetAttestationServiceClient(attestorAddr)
 	require.NoError(t, err, "unable to get attestation service client")
 
+	// 4. Deploy IBC contracts
+	out, err := base.EthChain.ForgeScript(evmDeployer, tv.E2EDeployScriptPath)
+	require.NoError(t, err, "unable to deploy ibc contracts")
+
+	evmContractWrappers := extractContractWrappers(t, out, base.EthChain.RPCClient)
+
+	// 5. Start the relayer
+	relayerClient := runRelayer(t, relayer.NewConfig([]relayer.ModuleConfig{
+		{
+			Name:     relayer.ModuleCosmosToEthAttested,
+			SrcChain: cosmosChain.Config().ChainID,
+			DstChain: evmChain.ChainID.String(),
+			Config: relayer.CosmosToEthAttestedModuleConfig{
+				AttestedChainId:  cosmosChain.Config().ChainID,
+				AggregatorConfig: relayer.DefaultAggregatorConfig(),
+				AttestedRpcUrl:   cosmosChain.GetHostRPCAddress(),
+				Ics26Address:     evmContractWrappers.ICS26RouterAddress.String(),
+				EthRpcUrl:        evmChain.RPC,
+			},
+		},
+	}))
+
 	// todo setup stuff
 	//    todo deploy cosmos LC on EVM
-	//    todo run a relayer
 	return &cosmosToEVMAttestorTestSuite{
 		T:    t,
 		Ctx:  ctx,
@@ -132,6 +175,9 @@ func newCosmosToEVMAttestorTestSuite(t *testing.T) *cosmosToEVMAttestorTestSuite
 		cosmosSender: cosmosSender,
 
 		attestorClient: attestorClient,
+		relayerClient:  relayerClient,
+
+		evmContracts: evmContractWrappers,
 	}
 }
 
@@ -162,4 +208,67 @@ func runAttestor(
 	})
 
 	return config.GetServerAddress()
+}
+
+func runRelayer(t *testing.T, config relayer.Config) relayertypes.RelayerServiceClient {
+	t.Helper()
+
+	err := config.GenerateConfigFile(tv.RelayerConfigFilePath)
+	require.NoError(t, err, "unable to generate relayer config file")
+
+	proc, err := relayer.StartRelayer(tv.RelayerConfigFilePath)
+	require.NoError(t, err, "unable to start relayer")
+
+	t.Cleanup(func() {
+		os.Remove(tv.RelayerConfigFilePath)
+		if err := proc.Kill(); err != nil {
+			t.Logf("unable to kill relayer process: %v", err)
+		}
+	})
+
+	client, err := relayer.GetGRPCClient(relayer.DefaultRelayerGRPCAddress())
+	require.NoError(t, err, "unable to get relayer client")
+
+	return client
+}
+
+type evmContracts struct {
+	ICS26RouterAddress ethcommon.Address
+	ICS26Router        *ics26router.Contract
+
+	ICS20TransferAddress ethcommon.Address
+	ICS20Transfer        *ics20transfer.Contract
+
+	ERC20Address ethcommon.Address
+	ERC20        *erc20.Contract
+}
+
+func extractContractWrappers(t *testing.T, raw []byte, evmClient *ethclient.Client) evmContracts {
+	t.Helper()
+
+	addresses, err := ethereum.GetEthContractsFromDeployOutput(string(raw))
+	require.NoError(t, err, "unable to parse eth contracts from deploy output")
+
+	ics26RouterAddress := ethcommon.HexToAddress(addresses.Ics26Router)
+	ics26Contract, err := ics26router.NewContract(ics26RouterAddress, evmClient)
+	require.NoError(t, err, "unable to create ics26 wrapper")
+
+	ics20TransferAddress := ethcommon.HexToAddress(addresses.Ics20Transfer)
+	ics20Contract, err := ics20transfer.NewContract(ics20TransferAddress, evmClient)
+	require.NoError(t, err, "unable to create ics20 wrapper")
+
+	erc20Address := ethcommon.HexToAddress(addresses.Erc20)
+	erc20Contract, err := erc20.NewContract(erc20Address, evmClient)
+	require.NoError(t, err, "unable to create erc20 wrapper")
+
+	return evmContracts{
+		ICS26RouterAddress: ics26RouterAddress,
+		ICS26Router:        ics26Contract,
+
+		ICS20TransferAddress: ics20TransferAddress,
+		ICS20Transfer:        ics20Contract,
+
+		ERC20Address: erc20Address,
+		ERC20:        erc20Contract,
+	}
 }
