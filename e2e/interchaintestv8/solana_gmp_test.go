563fd9919d84a6d186a63e18b2a6e9249a0622ff
Mariusz Zak <mariuszzak21@gmail.com>
2025-10-24 09:47:21 +0200

    extract gmp helpers

--- e2e/interchaintestv8/solana_gmp_test.go
+++ e2e/interchaintestv8/solana_gmp_test.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/cosmos/gogoproto/proto"
 	gmp_counter_app "github.com/cosmos/solidity-ibc-eureka/e2e/interchaintestv8/solana/go-anchor/gmpcounter"
-	bin "github.com/gagliardetto/binary"
 
 	solanago "github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/programs/token"
@@ -680,61 +679,6 @@ func (s *IbcEurekaSolanaTestSuite) Test_GMPSPLTokenTransferFromCosmos() {
 			cosmosUser.FormattedAddress(), ics27AccountPDA.String())
 		s.T().Logf("  Transferred %d tokens from ICS27 PDA to recipient", transferAmount)
 	}))
-}
-
-// AccountState represents the ICS27 GMP account state PDA
-// This mirrors the Rust struct in programs/solana/programs/ics27-gmp/src/state.rs
-type AccountState struct {
-	ClientID       string
-	Sender         string
-	Salt           []byte
-	Nonce          uint64
-	CreatedAt      int64
-	LastExecutedAt int64
-	ExecutionCount uint64
-	Bump           uint8
-}
-
-// getICS27AccountNonce retrieves the nonce from an ICS27 account state PDA
-// Returns 0 if account doesn't exist or cannot be read
-func (s *IbcEurekaSolanaTestSuite) getICS27AccountNonce(ctx context.Context, accountPDA solanago.PublicKey) uint64 {
-	accountInfo, err := s.SolanaChain.RPCClient.GetAccountInfo(ctx, accountPDA)
-	if err != nil || accountInfo.Value == nil {
-		return 0 // Account doesn't exist yet
-	}
-
-	data := accountInfo.Value.Data.GetBinary()
-	if len(data) < 8 {
-		return 0
-	}
-
-	// Create Borsh decoder and skip discriminator
-	decoder := bin.NewBorshDecoder(data)
-	_, err = decoder.ReadDiscriminator()
-	if err != nil {
-		return 0
-	}
-
-	// Decode AccountState using Borsh
-	var accountState AccountState
-	err = decoder.Decode(&accountState.ClientID)
-	if err != nil {
-		return 0
-	}
-	err = decoder.Decode(&accountState.Sender)
-	if err != nil {
-		return 0
-	}
-	err = decoder.Decode(&accountState.Salt)
-	if err != nil {
-		return 0
-	}
-	err = decoder.Decode(&accountState.Nonce)
-	if err != nil {
-		return 0
-	}
-
-	return accountState.Nonce
 }
 
 func (s *IbcEurekaSolanaTestSuite) Test_GMPSendCallFromSolana() {
@@ -1619,7 +1563,7 @@ func (s *IbcEurekaSolanaTestSuite) Test_GMPFailedExecutionFromCosmos() {
 		s.Require().Equal(uint64(0), initialDestBalance)
 
 		// Try to get initial nonce (will be 0 if account doesn't exist yet)
-		initialNonce = s.getICS27AccountNonce(ctx, ics27AccountPDA)
+		initialNonce = s.SolanaChain.GetICS27AccountNonce(ctx, ics27AccountPDA)
 
 		s.T().Logf("Initial state - Source: %d tokens, Dest: %d tokens, Nonce: %d",
 			initialSourceBalance, initialDestBalance, initialNonce)
