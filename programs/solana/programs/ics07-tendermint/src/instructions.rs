8956fcd551220b903e8e2d1201beda5d24867baf
Dmytro Onypko <vaporif@proton.me>
2025-10-24 19:21:34 +0100

    remove tendermint non chunked update

--- programs/solana/programs/ics07-tendermint/src/instructions.rs
+++ programs/solana/programs/ics07-tendermint/src/instructions.rs
@@ -2,7 +2,6 @@ pub mod assemble_and_update_client;
 pub mod cleanup_incomplete_upload;
 pub mod initialize;
 pub mod submit_misbehaviour;
-pub mod update_client;
 pub mod upload_header_chunk;
 pub mod verify_membership;
 pub mod verify_non_membership;
