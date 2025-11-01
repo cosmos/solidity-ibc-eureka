e30fec56951ab9e0e6ab41e12492c5c8566d59e7
srdtrk <srdtrk@hotmail.com>
2025-11-01 13:47:17 +0400

    test: remove electra fork test

--- e2e/interchaintestv8/relayer_test.go
+++ e2e/interchaintestv8/relayer_test.go
@@ -833,43 +833,6 @@ func (s *IbcEurekaTestSuite) ICS20FinalizedTimeoutPacketFromEthTest(
 	}))
 }
 
-func (s *RelayerTestSuite) Test_Electra_Fork() {
-	if os.Getenv(testvalues.EnvKeyEthTestnetType) != testvalues.EthTestnetTypePoS {
-		s.T().Skip("Test is only relevant for PoS networks")
-	}
-
-	ctx := context.Background()
-
-	electraForkEpoch := 12
-	chainconfig.KurtosisConfig.NetworkParams.ElectraForkEpoch = uint64(electraForkEpoch)
-
-	s.SetupSuite(ctx, operator.ProofTypeGroth16) // Doesn't matter, since we won't relay to eth in this test
-
-	s.FilteredRecvPacketToCosmosTest(ctx, 1, big.NewInt(testvalues.TransferAmount), nil)
-
-	spec, err := s.EthChain.BeaconAPIClient.GetSpec()
-	s.Require().NoError(err)
-	err = testutil.WaitForCondition(time.Minute*30, time.Second*30, func() (bool, error) {
-		finalityUpdate, err := s.EthChain.BeaconAPIClient.GetFinalityUpdate()
-		if err != nil {
-			return false, err
-		}
-
-		finalitySlot, err := strconv.Atoi(finalityUpdate.Data.FinalizedHeader.Beacon.Slot)
-		if err != nil {
-			return false, err
-		}
-
-		latestFinalityEpoch := uint64(finalitySlot) / spec.SlotsPerEpoch
-
-		fmt.Printf("Waiting for epoch %d, current epoch: %d\n", electraForkEpoch, latestFinalityEpoch)
-		return latestFinalityEpoch >= uint64(electraForkEpoch), nil
-	})
-	s.Require().NoError(err)
-
-	s.FilteredRecvPacketToCosmosTest(ctx, 1, big.NewInt(testvalues.TransferAmount), nil)
-}
-
 func (s *RelayerTestSuite) Test_10_RecvPacketToCosmos() {
 	ctx := context.Background()
 	s.SetupSuite(ctx, operator.ProofTypeGroth16) // Doesn't matter, since we won't relay to eth in this test
@@ -1371,7 +1334,6 @@ func (s *RelayerTestSuite) Test_Fulu_Fork() {
 	}
 
 	ctx := context.Background()
-	proofType := types.GetEnvProofType()
 
 	var fuluForkEpoch uint64
 	if chainconfig.GetKurtosisPreset() == testvalues.EnvValueEthereumPosPreset_Minimal {
@@ -1382,7 +1344,7 @@ func (s *RelayerTestSuite) Test_Fulu_Fork() {
 	}
 	chainconfig.KurtosisConfig.NetworkParams.FuluForkEpoch = fuluForkEpoch
 
-	s.SetupSuite(ctx, proofType)
+	s.SetupSuite(ctx, operator.ProofTypeGroth16)
 
 	s.FilteredRecvPacketToCosmosTest(ctx, 1, big.NewInt(testvalues.TransferAmount), nil)
 
