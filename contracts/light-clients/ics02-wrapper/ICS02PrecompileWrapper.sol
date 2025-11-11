55b077e98e998f4d2c07250335b42a298f1a2ae1
srdtrk <srdtrk@hotmail.com>
2025-11-11 15:43:16 +0400

    imp: fix style

--- contracts/light-clients/ics02-wrapper/ICS02PrecompileWrapper.sol
+++ contracts/light-clients/ics02-wrapper/ICS02PrecompileWrapper.sol
@@ -4,19 +4,20 @@ pragma solidity ^0.8.28;
 import { ILightClientMsgs } from "../../msgs/ILightClientMsgs.sol";
 import { IICS02ClientMsgs } from "../../msgs/IICS02ClientMsgs.sol";
 
+import { IICS02PrecompileWrapper } from "./interfaces/IICS02PrecompileWrapper.sol";
 import { IICS02PrecompileWrapperErrors } from "./errors/IICS02PrecompileWrapperErrors.sol";
 import { ILightClient } from "../../interfaces/ILightClient.sol";
 import { IICS02Precompile } from "./interfaces/IICS02Precompile.sol";
 
-/// @dev The ICS02I contract's address.
-address constant ICS02_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000000807;
+/// @title ICS02 Precompile Wrapper
+/// @notice A wrapper around the ICS02 Precompile contract to implement the ILightClient interface
+/// @dev This contract interacts with the ICS02 Precompile contract deployed at a fixed address in 'cosmos/evm'
+contract ICS02PrecompileWrapper is ILightClient, IICS02PrecompileWrapper, IICS02PrecompileWrapperErrors {
+    /// @notice The ICS02 Precompile contract
+    /// @dev The ICS02 Precompile contract is deployed at this fixed address in 'cosmos/evm'
+    IICS02Precompile constant private _ICS02_CONTRACT = IICS02Precompile(0x0000000000000000000000000000000000000807);
 
-/// @dev The ICS02 contract's instance.
-IICS02Precompile constant ICS02_CONTRACT = IICS02Precompile(ICS02_PRECOMPILE_ADDRESS);
-
-contract SP1ICS07Tendermint is ILightClient, IICS02PrecompileWrapperErrors {
-    /// @notice The client identifier of the IBC-Go Light Client
-    /// @dev The client-id associated to this light client in solidity-ibc may be different.
+    /// @inheritdoc IICS02PrecompileWrapper
     string public GO_CLIENT_ID;
 
     /// @notice The constructor sets the IBC-Go client identifier
@@ -27,12 +28,12 @@ contract SP1ICS07Tendermint is ILightClient, IICS02PrecompileWrapperErrors {
 
     /// @inheritdoc ILightClient
     function getClientState() external view returns (bytes memory) {
-        return ICS02_CONTRACT.getClientState(GO_CLIENT_ID);
+        return _ICS02_CONTRACT.getClientState(GO_CLIENT_ID);
     }
 
     /// @inheritdoc ILightClient
     function updateClient(bytes calldata updateMsg) external returns (ILightClientMsgs.UpdateResult) {
-        IICS02Precompile.UpdateResult result = ICS02_CONTRACT.updateClient(GO_CLIENT_ID, updateMsg);
+        IICS02Precompile.UpdateResult result = _ICS02_CONTRACT.updateClient(GO_CLIENT_ID, updateMsg);
 
         if (result == IICS02Precompile.UpdateResult.Update) {
             return ILightClientMsgs.UpdateResult.Update;
@@ -45,17 +46,17 @@ contract SP1ICS07Tendermint is ILightClient, IICS02PrecompileWrapperErrors {
 
     /// @inheritdoc ILightClient
     function verifyMembership(ILightClientMsgs.MsgVerifyMembership calldata msg_) external returns (uint256) {
-        return ICS02_CONTRACT.verifyMembership(GO_CLIENT_ID, msg_.proof, msg_.proofHeight, msg_.path, msg_.value);
+        return _ICS02_CONTRACT.verifyMembership(GO_CLIENT_ID, msg_.proof, msg_.proofHeight, msg_.path, msg_.value);
     }
 
     /// @inheritdoc ILightClient
     function verifyNonMembership(ILightClientMsgs.MsgVerifyNonMembership calldata msg_) external returns (uint256) {
-        return ICS02_CONTRACT.verifyNonMembership(GO_CLIENT_ID, msg_.proof, msg_.proofHeight, msg_.path);
+        return _ICS02_CONTRACT.verifyNonMembership(GO_CLIENT_ID, msg_.proof, msg_.proofHeight, msg_.path);
     }
 
     /// @inheritdoc ILightClient
-    function misbehaviour(bytes calldata updateMsg) external {
-        IICS02Precompile.UpdateResult result = ICS02_CONTRACT.updateClient(GO_CLIENT_ID, updateMsg);
+    function misbehaviour(bytes calldata misbehaviourMsg) external {
+        IICS02Precompile.UpdateResult result = _ICS02_CONTRACT.updateClient(GO_CLIENT_ID, misbehaviourMsg);
 
         if (result != IICS02Precompile.UpdateResult.Misbehaviour) {
             revert NoMisbehaviourDetected();
