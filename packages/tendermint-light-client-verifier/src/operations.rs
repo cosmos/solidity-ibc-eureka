bde2e5260091ea3c23bf99a02304d155b5ae3b62
Dmytro Onypko <vaporif@proton.me>
2025-11-18 16:34:24 +0000

    remove local, switch to forks

--- packages/tendermint-light-client-verifier/src/operations.rs
+++
@@ -1,7 +0,0 @@
-//! Crypto function traits allowing mocking out during testing
-
-pub mod voting_power;
-pub use self::voting_power::*;
-
-pub mod commit_validator;
-pub use self::commit_validator::*;
