a1b46f31d2355504386289b85300a97e481a3ec8
Dmytro Onypko <vaporif@proton.me>
2025-11-29 18:52:50 +0000

    simplify packets

--- packages/relayer/modules/cosmos-to-solana/src/tx_builder/packets.rs
+++ packages/relayer/modules/cosmos-to-solana/src/tx_builder/packets.rs
@@ -3,6 +3,7 @@
 use anchor_lang::prelude::*;
 use anyhow::Result;
 use solana_sdk::{
+    commitment_config::CommitmentConfig,
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
 };
@@ -17,6 +18,43 @@ use solana_ibc_types::{
 use super::TimeoutAccountsParams;
 use crate::constants::ANCHOR_DISCRIMINATOR_SIZE;
 
+/// Extracted payload info for recv packet processing.
+struct RecvPayloadInfo<'a> {
+    dest_port: &'a str,
+    encoding: &'a str,
+    value: &'a [u8],
+}
+
+/// Extract payload info from either `packet.payloads` or metadata + `payload_data`.
+fn extract_recv_payload_info<'a>(
+    msg: &'a MsgRecvPacket,
+    payload_data: &'a [Vec<u8>],
+) -> Result<RecvPayloadInfo<'a>> {
+    if msg.packet.payloads.is_empty() {
+        let [metadata] = msg.payloads.as_slice() else {
+            anyhow::bail!("Expected exactly one recv packet payload metadata element");
+        };
+        let value = payload_data
+            .first()
+            .ok_or_else(|| anyhow::anyhow!("Missing payload data"))?
+            .as_slice();
+        Ok(RecvPayloadInfo {
+            dest_port: &metadata.dest_port,
+            encoding: &metadata.encoding,
+            value,
+        })
+    } else {
+        let [payload] = msg.packet.payloads.as_slice() else {
+            anyhow::bail!("Expected exactly one recv packet payload element");
+        };
+        Ok(RecvPayloadInfo {
+            dest_port: &payload.dest_port,
+            encoding: &payload.encoding,
+            value: &payload.value,
+        })
+    }
+}
+
 impl super::TxBuilder {
     pub(crate) fn build_recv_packet_instruction(
         &self,
@@ -25,25 +63,10 @@ impl super::TxBuilder {
         chunk_accounts: Vec<Pubkey>,
         payload_data: &[Vec<u8>],
     ) -> Result<Instruction> {
-        let dest_port = if msg.packet.payloads.is_empty() {
-            let [metadata] = msg.payloads.as_slice() else {
-                return Err(anyhow::anyhow!(
-                    "Expected exactly one recv packet payload metadata element"
-                ));
-            };
-            &metadata.dest_port
-        } else {
-            let [payload] = msg.packet.payloads.as_slice() else {
-                return Err(anyhow::anyhow!(
-                    "Expected exactly one recv packet payload element"
-                ));
-            };
-            &payload.dest_port
-        };
+        let payload_info = extract_recv_payload_info(msg, payload_data)?;
 
         let (router_state, _) = RouterState::pda(self.solana_ics26_program_id);
-        let (ibc_app, _) = IBCApp::pda(dest_port, self.solana_ics26_program_id);
-
+        let (ibc_app, _) = IBCApp::pda(payload_info.dest_port, self.solana_ics26_program_id);
         let (packet_receipt, _) = Commitment::packet_receipt_pda(
             &msg.packet.dest_client,
             msg.packet.sequence,
@@ -55,16 +78,12 @@ impl super::TxBuilder {
             self.solana_ics26_program_id,
         );
         let (client, _) = Client::pda(&msg.packet.dest_client, self.solana_ics26_program_id);
-
         let (client_state, _) = ClientState::pda(chain_id, self.solana_ics07_program_id);
-
         let (consensus_state, _) =
             ConsensusState::pda(client_state, msg.proof.height, self.solana_ics07_program_id);
 
-        let ibc_app_program_id = self.resolve_port_program_id(dest_port)?;
-
-        let (ibc_app_state, _) = IBCAppState::pda(dest_port, ibc_app_program_id);
-
+        let ibc_app_program_id = self.resolve_port_program_id(payload_info.dest_port)?;
+        let (ibc_app_state, _) = IBCAppState::pda(payload_info.dest_port, ibc_app_program_id);
         let access_manager_program_id = self.resolve_access_manager_program_id()?;
         let (access_manager, _) = AccessManager::pda(access_manager_program_id);
 
@@ -85,32 +104,12 @@ impl super::TxBuilder {
             AccountMeta::new_readonly(client_state, false),
             AccountMeta::new_readonly(consensus_state, false),
         ];
-
-        for chunk_account in chunk_accounts {
-            accounts.push(AccountMeta::new(chunk_account, false));
-        }
-
-        let (dest_port_for_gmp, encoding, payload_value) = if msg.packet.payloads.is_empty() {
-            let metadata = &msg.payloads[0];
-            let data = &payload_data[0];
-            (
-                metadata.dest_port.as_str(),
-                metadata.encoding.as_str(),
-                data.as_slice(),
-            )
-        } else {
-            let payload = &msg.packet.payloads[0];
-            (
-                payload.dest_port.as_str(),
-                payload.encoding.as_str(),
-                payload.value.as_slice(),
-            )
-        };
+        accounts.extend(chunk_accounts.into_iter().map(|a| AccountMeta::new(a, false)));
 
         let gmp_accounts = gmp::extract_gmp_accounts(
-            dest_port_for_gmp,
-            encoding,
-            payload_value,
+            payload_info.dest_port,
+            payload_info.encoding,
+            payload_info.value,
             &msg.packet.dest_client,
             ibc_app_program_id,
         )?;
@@ -126,34 +125,18 @@ impl super::TxBuilder {
         })
     }
 
-    #[allow(clippy::cognitive_complexity, clippy::too_many_lines)]
     pub(crate) async fn build_ack_packet_instruction(
         &self,
         msg: &MsgAckPacket,
         chunk_accounts: Vec<Pubkey>,
     ) -> Result<Instruction> {
-        use solana_sdk::commitment_config::CommitmentConfig;
-
-        tracing::info!(
-            "Building ack packet instruction for packet from {} to {}, sequence {}",
-            msg.packet.source_client,
-            msg.packet.dest_client,
-            msg.packet.sequence
-        );
-
-        let solana_ics26_program_id = self.solana_ics26_program_id;
-
-        let (router_state, _) = RouterState::pda(solana_ics26_program_id);
-
         let [payload] = msg.packet.payloads.as_slice() else {
-            return Err(anyhow::anyhow!(
-                "Expected exactly one ack packet payload element"
-            ));
+            anyhow::bail!("Expected exactly one ack packet payload element");
         };
+        let source_port = &payload.source_port;
 
-        let source_port = payload.source_port.clone();
-
-        let (ibc_app_pda, _) = IBCApp::pda(&source_port, solana_ics26_program_id);
+        let (router_state, _) = RouterState::pda(self.solana_ics26_program_id);
+        let (ibc_app_pda, _) = IBCApp::pda(source_port, self.solana_ics26_program_id);
 
         let ibc_app_account = self
             .target_solana_client
@@ -163,42 +146,29 @@ impl super::TxBuilder {
             .ok_or_else(|| anyhow::anyhow!("IBC app account not found"))?;
 
         if ibc_app_account.data.len() < ANCHOR_DISCRIMINATOR_SIZE {
-            return Err(anyhow::anyhow!("Account data too short for IBCApp account"));
+            anyhow::bail!("Account data too short for IBCApp account");
         }
 
-        let mut data = &ibc_app_account.data[ANCHOR_DISCRIMINATOR_SIZE..];
-        let ibc_app = solana_ibc_types::IBCApp::deserialize(&mut data)
+        let mut account_data = &ibc_app_account.data[ANCHOR_DISCRIMINATOR_SIZE..];
+        let ibc_app = solana_ibc_types::IBCApp::deserialize(&mut account_data)
             .map_err(|e| anyhow::anyhow!("Failed to deserialize IBCApp account: {e}"))?;
-
         let ibc_app_program = ibc_app.app_program_id;
-        tracing::info!("IBC app program ID: {}", ibc_app_program);
 
-        let (app_state, _) = IBCAppState::pda(&source_port, ibc_app_program);
-
+        let (app_state, _) = IBCAppState::pda(source_port, ibc_app_program);
         let (packet_commitment, _) = Commitment::packet_commitment_pda(
             &msg.packet.source_client,
             msg.packet.sequence,
-            solana_ics26_program_id,
+            self.solana_ics26_program_id,
         );
-
-        let (client, _) = Client::pda(&msg.packet.source_client, solana_ics26_program_id);
-        tracing::info!(
-            "Router client PDA for '{}': {}",
-            msg.packet.source_client,
-            client
-        );
+        let (client, _) = Client::pda(&msg.packet.source_client, self.solana_ics26_program_id);
 
         let chain_id = self.chain_id().await?;
-        tracing::info!("Cosmos chain ID for ICS07 derivation: {}", chain_id);
-
         let (client_state, _) = ClientState::pda(&chain_id, self.solana_ics07_program_id);
-        tracing::info!("ICS07 client state PDA: {}", client_state);
-
         let (consensus_state, _) =
             ConsensusState::pda(client_state, msg.proof.height, self.solana_ics07_program_id);
 
         let access_manager_program_id = self.resolve_access_manager_program_id()?;
-        let (access_manager, _) = solana_ibc_types::AccessManager::pda(access_manager_program_id);
+        let (access_manager, _) = AccessManager::pda(access_manager_program_id);
 
         let mut accounts = vec![
             AccountMeta::new_readonly(router_state, false),
@@ -216,10 +186,7 @@ impl super::TxBuilder {
             AccountMeta::new_readonly(client_state, false),
             AccountMeta::new_readonly(consensus_state, false),
         ];
-
-        for chunk_account in chunk_accounts {
-            accounts.push(AccountMeta::new(chunk_account, false));
-        }
+        accounts.extend(chunk_accounts.into_iter().map(|a| AccountMeta::new(a, false)));
 
         let mut data = router_instructions::ack_packet_discriminator().to_vec();
         data.extend_from_slice(&msg.try_to_vec()?);
@@ -237,20 +204,9 @@ impl super::TxBuilder {
         msg: &MsgTimeoutPacket,
         chunk_accounts: Vec<Pubkey>,
     ) -> Result<Instruction> {
-        tracing::info!(
-            "Building timeout packet instruction for packet from {} to {}, sequence {}",
-            msg.packet.source_client,
-            msg.packet.dest_client,
-            msg.packet.sequence
-        );
-
         let source_port = Self::extract_timeout_source_port(msg)?;
-        let accounts = self.build_timeout_accounts_with_derived_keys(
-            chain_id,
-            msg,
-            &source_port,
-            chunk_accounts,
-        )?;
+        let accounts =
+            self.build_timeout_accounts_with_derived_keys(chain_id, msg, &source_port, chunk_accounts)?;
         let data = Self::build_timeout_instruction_data(msg)?;
 
         Ok(Instruction {
@@ -262,14 +218,11 @@ impl super::TxBuilder {
 
     pub(crate) fn extract_timeout_source_port(msg: &MsgTimeoutPacket) -> Result<String> {
         let [payload] = msg.packet.payloads.as_slice() else {
-            return Err(anyhow::anyhow!(
-                "Expected exactly one timeout packet payload element"
-            ));
+            anyhow::bail!("Expected exactly one timeout packet payload element");
         };
         Ok(payload.source_port.clone())
     }
 
-    #[allow(clippy::cognitive_complexity)]
     pub(crate) fn build_timeout_accounts_with_derived_keys(
         &self,
         chain_id: &str,
@@ -277,25 +230,23 @@ impl super::TxBuilder {
         source_port: &str,
         chunk_accounts: Vec<Pubkey>,
     ) -> Result<Vec<AccountMeta>> {
-        let program_id = self.solana_ics26_program_id;
-
-        let (router_state, _) = RouterState::pda(program_id);
-        let (ibc_app, _) = IBCApp::pda(source_port, program_id);
+        let (router_state, _) = RouterState::pda(self.solana_ics26_program_id);
+        let (ibc_app, _) = IBCApp::pda(source_port, self.solana_ics26_program_id);
         let (packet_commitment, _) = Commitment::packet_commitment_pda(
             &msg.packet.source_client,
             msg.packet.sequence,
-            program_id,
+            self.solana_ics26_program_id,
         );
 
         let ibc_app_program_id = self.resolve_port_program_id(source_port)?;
         let (ibc_app_state, _) = IBCAppState::pda(source_port, ibc_app_program_id);
-        let (client, _) = Client::pda(&msg.packet.source_client, program_id);
+        let (client, _) = Client::pda(&msg.packet.source_client, self.solana_ics26_program_id);
         let (client_state, _) = ClientState::pda(chain_id, self.solana_ics07_program_id);
         let (consensus_state, _) =
             ConsensusState::pda(client_state, msg.proof.height, self.solana_ics07_program_id);
 
         let access_manager_program_id = self.resolve_access_manager_program_id()?;
-        let (access_manager, _) = solana_ibc_types::AccessManager::pda(access_manager_program_id);
+        let (access_manager, _) = AccessManager::pda(access_manager_program_id);
 
         Ok(Self::assemble_timeout_accounts(TimeoutAccountsParams {
             access_manager,
@@ -331,11 +282,7 @@ impl super::TxBuilder {
             AccountMeta::new_readonly(params.client_state, false),
             AccountMeta::new_readonly(params.consensus_state, false),
         ];
-
-        for chunk_account in params.chunk_accounts {
-            accounts.push(AccountMeta::new(chunk_account, false));
-        }
-
+        accounts.extend(params.chunk_accounts.into_iter().map(|a| AccountMeta::new(a, false)));
         accounts
     }
 
