6914c00861af6967a839c3d89ab611e7e7c6f83d
srdtrk <srdtrk@hotmail.com>
2025-12-16 13:39:12 +0400

    feat: ownable ready

--- contracts/utils/IFTOwnableUpgradeable.sol
+++ contracts/utils/IFTOwnableUpgradeable.sol
@@ -2,8 +2,25 @@
 pragma solidity ^0.8.28;
 
 import { IFTBaseUpgradeable } from "./IFTBaseUpgradeable.sol";
-import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
+import { Initializable } from "@openzeppelin-upgradeable/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "@openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract IFTOwnable is IFTBaseUpgradeable, OwnableUpgradeable {
+/// @title IFT Ownable
+/// @notice This is the ownable and upgradable implementation of IFT
+contract IFTOwnable is IFTBaseUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @notice Initializes the IFTOwnable contract with the given owner
+    /// @param owner_ The owner of the contract
+    // natlint-disable-next-line MissingInheritdoc
+    function initialize(address owner_) external initializer {
+	__Ownable_init(owner_);
+    }
 
+    /// @inheritdoc IFTBaseUpgradeable
+    function _onlyAuthority() internal view override(IFTBaseUpgradeable) onlyOwner {}
+    // solhint-disable-previous-line no-empty-blocks
+
+    /// @inheritdoc UUPSUpgradeable
+    function _authorizeUpgrade(address) internal view override(UUPSUpgradeable) onlyOwner {}
+    // solhint-disable-previous-line no-empty-blocks
 }
