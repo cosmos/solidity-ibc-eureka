98617b46811a03b9c0871888dc896c59397f19d4
srdtrk <srdtrk@hotmail.com>
2026-01-15 17:58:45 +0400

    imp: adjusted decimal on IFT

--- contracts/utils/IFTAccessManaged.sol
+++ contracts/utils/IFTAccessManaged.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import { IFTBaseUpgradeable } from "./IFTBaseUpgradeable.sol";
 import { AccessManagedUpgradeable } from "@openzeppelin-upgradeable/access/manager/AccessManagedUpgradeable.sol";
 import { UUPSUpgradeable } from "@openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
+import { ERC20Upgradeable } from "@openzeppelin-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 /// @title IFT Access Managed
 /// @notice This is the access managed and upgradable implementation of IFT
@@ -41,4 +42,22 @@ contract IFTAccessManaged is IFTBaseUpgradeable, AccessManagedUpgradeable, UUPSU
     /// @inheritdoc UUPSUpgradeable
     function _authorizeUpgrade(address) internal override(UUPSUpgradeable) restricted { }
     // solhint-disable-previous-line no-empty-blocks
+
+    /**
+     * @inheritdoc ERC20Upgradeable
+     *
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Cosmos SDK tokens usually opt for a value of 6, imitating the relationship
+     * between ATOM and uatom.
+     *
+     * NOTE: This information is only used for _display_ purposes such as by wallets:
+     * it in no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public pure override(ERC20Upgradeable) returns (uint8) {
+        return 6;
+    }
 }
