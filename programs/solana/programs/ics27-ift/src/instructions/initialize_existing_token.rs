b469db682e78a920da5d7ab95588ef79674b87d3
Dmytro Onypko <vaporif@proton.me>
2026-01-28 03:26:03 +0000

    upd

--- programs/solana/programs/ics27-ift/src/instructions/initialize_existing_token.rs
+++ programs/solana/programs/ics27-ift/src/instructions/initialize_existing_token.rs
@@ -32,7 +32,7 @@ pub struct InitializeExistingToken<'info> {
     pub mint_authority: AccountInfo<'info>,
 
     #[account(
-        constraint = mint.mint_authority.unwrap() == current_authority.key() @ IFTError::InvalidMintAuthority
+        constraint = mint.mint_authority.map(|a| a == current_authority.key()).unwrap_or(false) @ IFTError::InvalidMintAuthority
     )]
     pub current_authority: Signer<'info>,
 
