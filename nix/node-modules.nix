4d7f25cff9493d58869506b594912b410047e476
Dmytro Onypko <vaporif@proton.me>
2026-01-30 13:37:00 +0000

    refact

--- nix/node-modules.nix
+++ nix/node-modules.nix
@@ -1,41 +1,38 @@
-{ pkgs }:
-
-let
+{pkgs}: let
   src = pkgs.lib.cleanSourceWith {
     src = ../.;
-    filter = path: type:
-      let
-        baseName = builtins.baseNameOf path;
-      in
-      baseName == "package.json"
+    filter = path: type: let
+      baseName = baseNameOf path;
+    in
+      baseName
+      == "package.json"
       || baseName == "bun.lock"
       || baseName == "bunfig.toml";
   };
-
 in
-pkgs.stdenv.mkDerivation {
-  pname = "node-modules";
-  version = "1.0.0";
+  pkgs.stdenv.mkDerivation {
+    pname = "node-modules";
+    version = "1.0.0";
 
-  inherit src;
+    inherit src;
 
-  nativeBuildInputs = [
-    pkgs.bun
-    pkgs.jq
-  ];
+    nativeBuildInputs = [
+      pkgs.bun
+      pkgs.jq
+    ];
 
-  buildPhase = ''
-    export HOME=$TMPDIR
-    export BUN_INSTALL_CACHE_DIR=$TMPDIR/bun-cache
-    bun install --frozen-lockfile --no-progress
-  '';
+    buildPhase = ''
+      export HOME=$TMPDIR
+      export BUN_INSTALL_CACHE_DIR=$TMPDIR/bun-cache
+      bun install --frozen-lockfile --no-progress
+    '';
 
-  installPhase = ''
-    mkdir -p $out
-    cp -R node_modules $out/
-  '';
+    installPhase = ''
+      mkdir -p $out
+      cp -R node_modules $out/
+    '';
 
-  outputHashMode = "recursive";
-  outputHashAlgo = "sha256";
-  outputHash = "sha256-dXMK+OyDvBmG3dFngt6nFaIZ02q2bqOUrNZqbOLmrNk=";
-}
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
+    outputHash = "sha256-dXMK+OyDvBmG3dFngt6nFaIZ02q2bqOUrNZqbOLmrNk=";
+  }
