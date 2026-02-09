bc2a1988511a93417af41c1f8de71cdb38e8efea
Dmytro Onypko <vaporif@proton.me>
2026-02-09 20:34:00 +0900

    upd

--- programs/solana/programs/ift/src/instructions/create_spl_token.rs
+++ programs/solana/programs/ift/src/instructions/create_spl_token.rs
@@ -19,6 +19,7 @@ pub struct CreateSplToken<'info> {
     )]
     pub app_state: Account<'info, IFTAppState>,
 
+    // TODO: symbol & metadata where is it set here??
     // TODO: does it set the same for freeze and metadata???
     /// SPL Token mint (created by IFT with PDA as authority)
     #[account(
